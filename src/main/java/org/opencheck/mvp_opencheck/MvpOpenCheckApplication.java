e66e59a56a0be8ef02b05acc328c10cb4df4aebc
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-06-03 18:02:58 +0200

    Adding a API-REST interface to the project.

--- src/main/java/org/opencheck/mvp_opencheck/MvpOpenCheckApplication.java
+++ src/main/java/org/opencheck/mvp_opencheck/MvpOpenCheckApplication.java
@@ -2,6 +2,24 @@ package org.opencheck.mvp_opencheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class MvpOpenCheckController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/{name}")
+	public String greeting(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}
 
 @SpringBootApplication
 public class MvpOpenCheckApplication {
