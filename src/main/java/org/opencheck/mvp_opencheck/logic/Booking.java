130416eb425b3db5c15317e62890398613ab7b55
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-06-03 18:06:55 +0200

    Adding an extra constructor to the class. Adding the attribute **extraActivities** as a Bill to store extra property activities or services.

--- src/main/java/org/opencheck/mvp_opencheck/logic/Booking.java
+++ src/main/java/org/opencheck/mvp_opencheck/logic/Booking.java
@@ -11,18 +11,41 @@ import java.util.Map;
 @AllArgsConstructor
 public abstract class Booking {
 
-    //TODO check implementation.
-
     private String id;
+
     private Guest guest;
     private List<Room> rooms;
-
     private LocalDateTime dateIn;
     private LocalDateTime dateOut;
 
-    private Map<String, Double> extraActivities;
     private double finalBill;
+    private Bill extraActivities;
     private boolean paymentState;
+
+    public Booking(Guest guest, LocalDateTime dateIn, LocalDateTime dateOut, List<Room> rooms, Bill extraActivities) {
+        this.guest = guest;
+        this.rooms = rooms;
+        this.dateIn = dateIn;
+        this.dateOut = dateOut;
+        this.extraActivities = extraActivities;
+
+        this.finalBill = calculateFinalBill();
+    }
+
+    private double calculateFinalBill(){
+        double finalBill = 0.0;
+        for (Room room : this.rooms){
+            finalBill += room.getFinalPrice();
+        }
+
+        finalBill += extraActivities.getFinalAmount();
+
+        return finalBill;
+    }
+
+    public void updateBooking(){
+        this.finalBill = calculateFinalBill();
+    }
 
     //APIs-related Methods
     abstract boolean validateBooking();
