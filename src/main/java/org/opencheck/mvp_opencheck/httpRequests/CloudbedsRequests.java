4b1975ffdbe218920c8334a7140e676371473a6d
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-06-14 20:15:58 +0200

    Adding new methods to CloudbedsRequests.java:  * requestAccessTokenByScopes  * getHotelDetails
     Adding some doc-string.

--- src/main/java/org/opencheck/mvp_opencheck/httpRequests/CloudbedsRequests.java
+++ src/main/java/org/opencheck/mvp_opencheck/httpRequests/CloudbedsRequests.java
@@ -1,28 +1,25 @@
 package org.opencheck.mvp_opencheck.httpRequests;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+
 public class CloudbedsRequests extends Request{
 
-    private String clientId;
-    private String propertyId;
-    private String clientSecret;
-    private String redirectUri;
+    private final String  clientId;
+    private final String propertyId;
+    private final String clientSecret;
+    private final String redirectUri;
     private String code;
 
     private String state;
@@ -44,6 +41,34 @@ public class CloudbedsRequests extends Request{
     }
 
     //Getter-Setter section
+    public String requestAccessTokenByScopes(@NotNull List<String> scopes){
+
+        try {
+
+            StringBuilder bld = new StringBuilder();
+            for (String temp: scopes) {
+                bld.append(temp + " ");
+            }
+            String urlScopes = bld.toString();
+
+            URL url = new URL("https://hotels.cloudbeds.com/api/v1.1/oauth?scope=" + urlScopes);
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("client_id", this.clientId);
+            parameters.put("redirect_uri", this.redirectUri);
+            parameters.put("response_type", "code");
+
+            JSONObject pmsResponse = Request.httpRequest(url, "GET", null, null);
+            this.code = (String) pmsResponse.get("code");
+            this.state = (String) pmsResponse.get("state");
+            
+        } catch (MalformedURLException |  JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return this.code;
+    }
+
     public String getAccessToken() {
 
         try {
@@ -55,8 +80,8 @@ public class CloudbedsRequests extends Request{
             parameters.put("redirect_uri", this.redirectUri);
 
             if (this.refreshToken != null){
-                parameters.put("refresh_token", this.refreshToken);
-                parameters.put("grant_type", "refresh_token");
+                parameters.put(REFRESH_TOKEN, this.refreshToken);
+                parameters.put("grant_type", REFRESH_TOKEN);
             }else {
                 parameters.put("code", this.code);
                 parameters.put("grant_type", "authorization_code");
@@ -65,13 +90,13 @@ public class CloudbedsRequests extends Request{
 
             JSONObject pmsResponse = Request.httpRequest(url, "POST", parameters, null);
             this.accessToken = (String) pmsResponse.get("access_token");
-            this.refreshToken = (String) pmsResponse.get("refresh_token");
+            this.refreshToken = (String) pmsResponse.get(REFRESH_TOKEN);
 
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
 
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         list.add("Authorization");
         list.add("Bearer " + this.accessToken);
         this.header = list;
@@ -82,7 +107,7 @@ public class CloudbedsRequests extends Request{
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
 
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         list.add("Authorization");
         list.add("Bearer " + this.accessToken);
         this.header = list;
@@ -100,6 +125,22 @@ public class CloudbedsRequests extends Request{
     public JSONObject getUsrInfo(){
         JSONObject pmsResponse;
         try {
+            URL url = new URL("https://hotels.cloudbeds.com/api/v1.1/userinfo");
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("property_id", this.propertyId);
+
+            pmsResponse = Request.httpRequest(url, "GET", parameters, this.header);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return pmsResponse;
+    }
+
+    public JSONObject getHotelDetails(){
+        JSONObject pmsResponse;
+        try {
             URL url = new URL("https://hotels.cloudbeds.com/api/v1.1/getHotelDetails");
 
             Map<String, String> parameters = new HashMap<>();
@@ -112,4 +153,43 @@ public class CloudbedsRequests extends Request{
         }
         return pmsResponse;
     }
+
+    /**
+     * Returns a list of reservations that matched the filters criteria.
+     * Please note that some reservations modification may not be reflected in this timestamp.
+     *
+     * <p> Filters criteria:
+     * <ol>
+     *     <li>  status - A {@code String}. Filter by current reservation status. Allowed values: "not_confirmed", "confirmed", "canceled", "checked_in", "checked_out", "no_show"
+     *     <li>  checkInFrom/checkInTo -  A {@code DataTime}.
+     *     <li>  checkOutFrom/checkOutTo - A {@code DataTime}.
+     * </ol>
+     *
+     *<p>Required Scopes:
+     * <ul>
+     * <li>{@code "scope=read:reservation"}.</li>
+     * </ul>
+     *
+     * @param criteria The {@code Map<String, String>} with the filters criteria.
+     * @return a JSON object with the reservation requested.
+     *
+     * @author José Manuel Ramírez
+     * @author Edward L. Campbell
+     */
+    public JSONObject getReservations(@NotNull Map<String, String> criteria){
+        JSONObject pmsResponse;
+        criteria.put("propertyID", this.propertyId);
+
+        try {
+            URL url = new URL("https://hotels.cloudbeds.com/api/v1.1/getReservations");
+            pmsResponse = Request.httpRequest(url, "GET", criteria, this.header);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return pmsResponse;
+    }
+
+    private static final String REFRESH_TOKEN = "refresh_token";
 }
