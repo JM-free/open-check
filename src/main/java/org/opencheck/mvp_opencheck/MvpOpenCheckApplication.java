101fbf79d01e19e51a359f176cd2f84bbe65fe26
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-06-14 20:15:30 +0200

    New Pipe-line

--- src/main/java/org/opencheck/mvp_opencheck/MvpOpenCheckApplication.java
+++ src/main/java/org/opencheck/mvp_opencheck/MvpOpenCheckApplication.java
@@ -1,36 +1,33 @@
 package org.opencheck.mvp_opencheck;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.opencheck.mvp_opencheck.httpRequests.CloudbedsRequests;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MvpOpenCheckApplication {
 
-	public static void main(String[] args) throws JSONException, IOException, InterruptedException {
+	public static void main(String[] args) {
 
 		String clientId = "live1_212599_LBRQd0lhTFIkJH5GCpwPUOEg";
 		String clientSecret = "gwSvTrFauEbt4OkKy1sVGCnB3hpcjf7I";
 		String propertyId = "212599";
 		String redirectUri = "https://81ee-31-4-128-46.eu.ngrok.io";
+		String code = "-MrRQx3QejusB1w0TVGy8S2tGcLju3Ysem0RCV2oKWk";
+		String state = "fb08f0d2ff44e959fcd83dc20e58a8c0d729cf6f62a8937cf0b19";
 
-		String accessToken = "eyJraWQiOiJsVXJsdTNvUjVVRTlIb2YyNXRMNHNfTzRXNEF6Wll4akMzSmJjUXZ1VEdZIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULkRTdmV2R2pxWnUtd2xTdEJZTlBoWlFoUWRmZ3R6a0FVY0F0R1FyRUctdmcub2FyaTVkdDU2akZhVWhZWWE1ZDYiLCJpc3MiOiJodHRwczovL2lkcC5jbG91ZGJlZHMuY29tL29hdXRoMi9hdXNkNWcydTY5QmxKNFdBYzVkNiIsImF1ZCI6Imh0dHBzOi8vaG90ZWxzLmNsb3VkYmVkcy5jb20vYXBpIiwiaWF0IjoxNjU1MTQ2OTM5LCJleHAiOjE2NTUxNTA1MzksImNpZCI6ImxpdmUxXzIxMjU5OV9MQlJRZDBsaFRGSWtKSDVHQ3B3UFVPRWciLCJ1aWQiOiIwMHU1MHY0ZTNmT1Y1VlJuVzVkNyIsInNjcCI6WyJvZmZsaW5lX2FjY2VzcyIsInJlYWQ6aG90ZWwiXSwiYXV0aF90aW1lIjoxNjU0NzgwMTA0LCJzdWIiOiJyYW1pcmV6c2FuY2hlempvc2VtQGdtYWlsLmNvbSIsInR5cGUiOiJwcm9wZXJ0eSJ9.kbROfT87I1e4o3OXyAw1SAWfw1o483riC71uEnEy2lhS_VCCbiBhKt6ZhNN3BLWt3BmnJYpVyJrJIlimyLTYw9V04PkUgdOnqet64hFS09hppGRPtqM0yVEuhGhmyIhDP-0FyXPhCygLIm0eNK20T-Cu_Gp5yorgD4Y0ZqxegWrttBrDEJHWzXg4O8rW286NRsx_wjwi9Vo3cGOKVgHZm68n6FdFCMU-Ae-xWQyx_iIVxNvjPqOJOf1-8IbGcrfYOpko_CQSlhxDHW-hU3JOsLF6R4dFEflotvQAgWuH4kBoXNNIByZrsDe0W7smWSgCUR6JxAsMm2aI6uqj5pxW_w";
-		String refreshToken = "x9kq-gDVrmnscyDjV_dp7mLNOtjAHK9rz9nnb3KY4m8";
+		String refreshToken = "fzfOngPJkMuwz45Jz6Lhtbd_ab5LSqv08Lm-xFJovcQ";
 
-		CloudbedsRequests rq = new CloudbedsRequests(propertyId, clientId, clientSecret, redirectUri, "", "");
-		rq.setAccessToken(accessToken);
+		CloudbedsRequests rq = new CloudbedsRequests(propertyId, clientId, clientSecret, redirectUri, code, state);
 		rq.setRefreshToken(refreshToken);
 		String ans = rq.getAccessToken();
 		System.out.println(ans);
 		ans = String.valueOf(rq.getUsrInfo());
+		System.out.println(ans);
+
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("status", "confirmed");
+		ans = String.valueOf(rq.getReservations(parameters));
 		System.out.println(ans);
 	}
 }
