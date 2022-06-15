0df3508c7f4d54a72aac19bcb47dadb7b4a3ac18
Jose M. Ramirez <ramirezsanchezjosem@gmail.com>
2022-06-15 13:42:02 +0200

    Imports and Package update.

--- src/main/java/org/opencheck/mvp/requests/Request.java
+++ src/main/java/org/opencheck/mvp/requests/Request.java
@@ -1,72 +1,75 @@
-package org.opencheck.mvp_opencheck.httpRequests;
+package org.opencheck.mvp.requests;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.*;
 
-import java.net.MalformedURLException;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
-import java.util.Iterator;
 
 import java.net.URL;
-import java.net.URLEncoder;
 import java.net.HttpURLConnection;
 
-public class Request {
+class Request {
+
+    private Request() {
+    }
+
     /**
+     * Return a {{@code @JSONObject}} from the {@code BufferReader} of a URL.
      *
-     * @param bufferedReader
-     * @return
-     * @throws IOException
-     * @throws JSONException
+     * @param bufferedReader - The {@code BufferReader} of the URL.
+     * @return - The {@code JSONObject} from the {@code BufferReader}.
+     * @throws IOException - If an error occurs while reading the {@code BufferReader}.
+     * @throws JSONException - If an error occurs while parsing the {{@code @JSONObject}}.
      */
     @NotNull
     private static JSONObject httpResponseBufferToJson(BufferedReader bufferedReader) throws IOException, JSONException {
         StringBuilder bld = new StringBuilder();
         String stringLine;
         while ((stringLine = bufferedReader.readLine()) != null) {
-            bld.append(stringLine + "\n");
+            bld.append(stringLine).append("\n");
         }
         String response = bld.toString();
 
         JSONTokener tokener = new JSONTokener(response);
-        JSONObject json = new JSONObject(tokener);
-        return json;
+        return new JSONObject(tokener);
     }
 
     /**
+     * Return a String with all the {@code HttpURLConnection} headers and {@code HttpURLConnection} parameters.
      *
-     * @param parameters
-     * @return
+     * @param parameters - The {@code Map} of the parameters.
+     * @return - The String with all the {@code HttpURLConnection} headers and {@code HttpURLConnection} parameters.
      */
-    @Nullable
-    private static String getRequestedData(@NotNull Map<String, String> parameters) {
-        String requestedData = "";
+    private static @NotNull String getRequestedData(@NotNull Map<String, String> parameters) {
+
+        boolean fristElement = true;
+        StringBuilder bld = new StringBuilder();
+
         for (Map.Entry<String,String> entry : parameters.entrySet()){
             String parameter =  entry.getKey();
             String value = entry.getValue();
 
-            if (requestedData.isEmpty()) {
-                requestedData += parameter + "=" + value;
+            if (fristElement) {
+                bld.append(parameter).append("=").append(value);
+                fristElement = false;
             }else{
-                requestedData += "&" + parameter + "=" + value;
+                bld.append("&").append(parameter).append("=").append(value);
             }
 
         }
-        return requestedData;
+        return bld.toString();
     }
 
     /**
+     * Return the {@code JSONObject} from an HTTP Request.
      *
-     * @param requestedUrl
-     * @param requestMethod
-     * @param parameters
+     * @param requestedUrl - The URL of the request.
+     * @param requestMethod - The method of the request.
+     * @param parameters - The {@code Map} of the parameters.
      */
     protected static JSONObject httpRequest(@NotNull URL requestedUrl, String requestMethod, Map<String, String> parameters, List<String> header) {
 
